03594478344a5548e7026b55441ce9849e86bd3c
Antony <elmagnificopollo@gmail.com>
2021-03-12 19:26:43 -0500

    Creacion de la vista inicio

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -21,6 +21,30 @@
             <i class="contacto">Contacto: </i>
         </p>
     </div>
+    <div class="cuerpo">
+      <div class="lateral">
+        <div class="tarjeta">
+          <center><div class="titulo-datos">DATOS</div></center>
+          <div class="datos">
+            <div class="label">Nombre:</div><div class="info">Antony</div>
+            <div class="label">Grado actual:</div><div class="info">Egresado de ingenieria de sistemas e informatica</div>
+            <div class="label">Universidad de procedencia:</div><div class="info">Universidad Alas Peruanas</div>
+            <div class="label">Ubicacion:</div><div class="info">Cusco</div>
+            <div class="label">Edad actual:</div><div class="info">24 años</div>
+          </div>
+        </div>
+        <div class="tarjeta"></div>
+      </div>
+      <div class="tarjeta conocimientos">
+        <div class="titulo-datos titulo-conocimiento">CONOCIMIENTOS</div>
+        <div class="label">Ofimatica:</div><div class="info">Google (Docs, Meets, Drive, Classroom, Sheets, Slides, Forms), Microsoft (Word, Excel, Powerpoint, Publisher, Access, Outlook, OneDrive, Teams, Stream), Libre Office. </div>
+        <div class="label">Sistemas operativos:</div><div class="info">Windows XP, 7, 8, 10, Server (avanzado), Linux Ubuntu, CentOS, Kubuntu, Lubuntu, Debian (intermedio).</div>
+        <div class="label">Programacion web:</div><div class="info">Javascript (intermedio), HTML (avanzado), CSS (avanzado), PHP (basico),ReactJS (basico).</div>
+        <div class="label">Desarrollo de software:</div><div class="info">C# (internmedio), Java (intermedio), Python (Intermedio).</div>
+        <div class="label">IDEs:</div><div class="info">Visual Studio 2019, Visual Studio Code, Netbeens, MySQL Workbench, SQL Server, Pycharm, IntelliJ IDEA.</div>
+        <div class="label">Software especializado:</div><div class="info">Chocolatey (Gestor de paquetes para instalacion de softwares en Windows desde terminal), VirtualBox, Multipass, VMware Workstation, WSL(version 1 y 2), TeamViewer, Anydesk, OBS Studio, Photoshop (basico), Illustrator (basico). </div>
+      </div>
+    </div>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
